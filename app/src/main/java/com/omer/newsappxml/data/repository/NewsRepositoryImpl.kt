6875e44b944c6502565e7182852dccedaaba0910
omer <omerakpul38@gmail.com>
2025-07-11 17:46:49 +0300

    Implement repository pattern - Added NewsRepository interface (domain) - Added NewsRepositoryImpl (data) - Repository manages data source selection and model mapping

--- app/src/main/java/com/omer/newsappxml/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/omer/newsappxml/data/repository/NewsRepositoryImpl.kt
@@ -1,4 +1,44 @@
 package com.omer.newsappxml.data.repository
 
-class NewsRepositoryImpl {
+import com.omer.newsappxml.data.local.NewsLocalDataSource
+import com.omer.newsappxml.data.mapper.toDomain
+import com.omer.newsappxml.data.mapper.toEntity
+import com.omer.newsappxml.data.remote.NewsRemoteDataSource
+import com.omer.newsappxml.domain.model.News
+import com.omer.newsappxml.domain.repository.NewsRepository
+import javax.inject.Inject
+
+class NewsRepositoryImpl @Inject constructor(
+    private val localDataSource: NewsLocalDataSource,
+    private val remoteDataSource: NewsRemoteDataSource
+) : NewsRepository {
+
+    override suspend fun getNews(country: String, category: String): List<News> {
+        val localEntities = localDataSource.getNews(country, category)
+        if (localEntities.isNotEmpty()) {
+            return localEntities.map { it.toDomain() }
+        } else {
+            val remoteDtos = remoteDataSource.fetchNews(country, category)
+            val entities = remoteDtos.map { it.toEntity(country,category)}
+            localDataSource.insertNews(entities)
+            return localDataSource.getNews(country, category).map { it.toDomain() }
+        }
+    }
+
+    override suspend fun refreshNews(country: String, category: String) {
+        val remoteDtos = remoteDataSource.fetchNews(country, category)
+        val entities = remoteDtos.map {it.toEntity(country, category)}
+        localDataSource.deleteNews(country, category)
+        localDataSource.insertNews(entities)
+    }
+
+    override suspend fun searchNews(query: String): List<News> {
+        val allEntities = localDataSource.getAllNews()
+        val filtered = allEntities.filter {
+            it.title?.contains(query, true) == true ||
+                    it.description?.contains(query, true) == true }
+        return filtered.map { it.toDomain() }
+    }
+
+
 }
--- app/src/main/java/com/omer/newsappxml/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/omer/newsappxml/domain/repository/NewsRepository.kt
@@ -1,4 +1,9 @@
 package com.omer.newsappxml.domain.repository
 
-class NewsRepository {
+import com.omer.newsappxml.domain.model.News
+
+interface NewsRepository {
+    suspend fun getNews(country: String, category: String): List<News>
+    suspend fun refreshNews(country: String, category: String)
+    suspend fun searchNews(query: String): List<News>
 }
