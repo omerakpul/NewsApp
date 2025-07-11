eeca84690d215ec0404da49d88b03947f3b78afe
omer <omerakpul38@gmail.com>
2025-07-11 17:44:40 +0300

    Implemented extension functions to map ApiNewsDto to NewsEntity.

--- app/src/main/java/com/omer/newsappxml/data/mapper/NewsMapper.kt
+++ app/src/main/java/com/omer/newsappxml/data/mapper/NewsMapper.kt
@@ -1,9 +1,8 @@
 package com.omer.newsappxml.data.mapper
 
+import com.omer.newsappxml.data.remote.dto.ApiNewsDto
 import com.omer.newsappxml.domain.model.News
 import com.omer.newsappxml.data.model.News as NewsEntity
-
-class NewsMapper {
 
     fun NewsEntity.toDomain(): News = News(
         url = url,
@@ -27,4 +26,15 @@ class NewsMapper {
         country = country
     )
 
-}
+    fun ApiNewsDto.toEntity(country: String, category: String, ): NewsEntity {
+        return NewsEntity(
+            url = this.url,
+            author = this.author,
+            title = this.title,
+            description = this.description,
+            urlToImage = this.urlToImage,
+            content = this.content,
+            category = category,
+            country = country
+        )
+    }
