92ac7704b9f4cabe9440203a1f6a7238d387bf41
omer <omerakpul38@gmail.com>
2025-07-11 17:05:04 +0300

    implemented NewsLocalDataSource using DAO as local data provider

--- app/src/main/java/com/omer/newsappxml/data/local/NewsLocalDataSource.kt
+++ app/src/main/java/com/omer/newsappxml/data/local/NewsLocalDataSource.kt
@@ -1,4 +1,23 @@
 package com.omer.newsappxml.data.local
 
-class NewsLocalDataSource {
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.omer.newsappxml.data.model.News as NewsEntity
+import javax.inject.Inject
+
+class NewsLocalDataSource @Inject constructor(
+    private val newsDAO: NewsDAO
+) {
+
+    suspend fun getNews(country: String, category: String): List<NewsEntity> =
+        newsDAO.getFilteredNews(country, category)
+
+    suspend fun deleteNews(country: String, category: String) =
+        newsDAO.deleteFilteredNews(country, category)
+
+    suspend fun insertNews(news: List<NewsEntity>) =
+        newsDAO.insertAll(news)
+
+    suspend fun getAllNews() : List<NewsEntity> =
+        newsDAO.getAllNews()
 }
