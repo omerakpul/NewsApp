babc660fca6d5b0bb653bf731474f90c5ee46ae8
omer <omerakpul38@gmail.com>
2025-07-11 17:10:36 +0300

    implemented NewsRemoteDataSource using DAO as remote data provider

--- app/src/main/java/com/omer/newsappxml/data/remote/NewsRemoteDataSource.kt
+++ app/src/main/java/com/omer/newsappxml/data/remote/NewsRemoteDataSource.kt
@@ -1,4 +1,12 @@
 package com.omer.newsappxml.data.remote
 
-class NewsRemoteDataSource {
+import com.omer.newsappxml.data.remote.dto.ApiNewsDto
+import javax.inject.Inject
+
+class NewsRemoteDataSource @Inject constructor(
+    private val apiService: NewsAPIService
+) {
+    suspend fun fetchNews(country: String, category: String): List<ApiNewsDto> {
+        apiService.getDataByCategory(country, category)
+    }
 }
