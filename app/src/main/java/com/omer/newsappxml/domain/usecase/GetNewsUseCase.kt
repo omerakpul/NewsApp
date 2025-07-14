65a18e00fa0f967d05ea2be1d33c508848f5ac6b
omer <omerakpul38@gmail.com>
2025-07-14 10:20:29 +0300

    Added use cases for getting, refreshing, and searching news

--- app/src/main/java/com/omer/newsappxml/domain/usecase/GetNewsUseCase.kt
+++ app/src/main/java/com/omer/newsappxml/domain/usecase/GetNewsUseCase.kt
@@ -1,10 +1,10 @@
 package com.omer.newsappxml.domain.usecase
 
 import com.omer.newsappxml.domain.repository.NewsRepository
+import com.omer.newsappxml.domain.model.News
 
 class GetNewsUseCase(private val repo : NewsRepository) {
-
-    suspend operator fun invoke(country:String, category:String){
-        repo.getNews(country,category)
+    suspend operator fun invoke(country:String, category:String): List<News> {
+        return repo.getNews(country, category)
     }
 }
--- app/src/main/java/com/omer/newsappxml/domain/usecase/SearchNewsUseCase.kt
+++ app/src/main/java/com/omer/newsappxml/domain/usecase/SearchNewsUseCase.kt
@@ -1,9 +1,10 @@
 package com.omer.newsappxml.domain.usecase
 
+import com.omer.newsappxml.domain.model.News
 import com.omer.newsappxml.domain.repository.NewsRepository
 
 class SearchNewsUseCase(private val repo : NewsRepository) {
-    suspend operator fun invoke(query: String){
-        repo.searchNews(query)
+    suspend operator fun invoke(query: String): List<News> {
+        return repo.searchNews(query)
     }
 }
