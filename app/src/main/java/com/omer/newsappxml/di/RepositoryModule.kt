5d83178713dfb212e2f4dc9f145855889edd095d
omer <omerakpul38@gmail.com>
2025-07-14 12:29:59 +0300

    Provided NewsRepository implementation via Hilt RepositoryModule

--- app/src/main/java/com/omer/newsappxml/di/RepositoryModule.kt
+++ app/src/main/java/com/omer/newsappxml/di/RepositoryModule.kt
@@ -1,4 +1,19 @@
 package com.omer.newsappxml.di
 
-class RepositoryModule {
+import com.omer.newsappxml.data.repository.NewsRepositoryImpl
+import com.omer.newsappxml.domain.repository.NewsRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindNewsRepository(
+        newsRepositoryImpl: NewsRepositoryImpl
+    ) : NewsRepository
 }
