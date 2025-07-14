4345ea71dac74c4c964c4d577efbfaeea128ae28
omer <omerakpul38@gmail.com>
2025-07-14 15:58:25 +0300

    Added back button to NewsDetailsFragment

--- app/src/main/java/com/omer/newsappxml/presentation/ui/details/NewsDetailsFragment.kt
+++ app/src/main/java/com/omer/newsappxml/presentation/ui/details/NewsDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import coil.load
 import com.omer.newsappxml.databinding.FragmentNewsDetailsBinding
 import com.omer.newsappxml.domain.model.News
@@ -18,7 +19,6 @@ class NewsDetailsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -32,21 +32,21 @@ class NewsDetailsFragment : Fragment() {
 
         val news = arguments?.getSerializable("news") as? News
 
-
         if (news != null) {
-            binding.newsDetailTitle.text = news.title
-            binding.newsDetailImageView.load(news.urlToImage)
-            binding.newsDetailDescription.text = news.description
+            binding.newsTitle.text = news.title
+            binding.newsImageView.load(news.urlToImage)
+            binding.newsDescription.text = news.description
         } else {
-            binding.newsDetailTitle.text = "News could not found."
+            binding.newsTitle.text = "News could not found."
         }
 
-
+        binding.backButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
