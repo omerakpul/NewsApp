d5730ed7260c1a0606c5feb02325232e8a22ae0d
omer <omerakpul38@gmail.com>
2025-07-17 16:55:47 +0300

    Formatted viewmodel code

--- app/src/main/java/com/omer/newsappxml/presentation/viewmodel/NewsHomeViewModel.kt
+++ app/src/main/java/com/omer/newsappxml/presentation/viewmodel/NewsHomeViewModel.kt
@@ -13,34 +13,34 @@ import javax.inject.Inject
 
 @HiltViewModel
 class NewsHomeViewModel @Inject constructor(
-    private val newsUseCases : NewsUseCases,
+    private val newsUseCases: NewsUseCases,
 ) : ViewModel() {
 
     private val _newsState = MutableLiveData<NewsUiState>()
     val newsState: LiveData<NewsUiState> = _newsState
 
-        fun getNews(country:String, category: String, fromInternet : Boolean){
-            viewModelScope.launch {
-                _newsState.value= NewsUiState.Loading
-                try {
-                    val result = newsUseCases.getNews(country, category, fromInternet)
-                    _newsState.value = NewsUiState.Success(result)
-                } catch (e:Exception) {
-                    _newsState.value = NewsUiState.Error(e.message)
-                }
-            }
-        }
-
-
-    fun searchNews(query:String,country: String, category: String) {
+    fun getNews(country: String, category: String, fromInternet: Boolean) {
         viewModelScope.launch {
             _newsState.value = NewsUiState.Loading
             try {
-                val result = newsUseCases.searchNews(query,country, category)
+                val result = newsUseCases.getNews(country, category, fromInternet)
                 _newsState.value = NewsUiState.Success(result)
             } catch (e: Exception) {
                 _newsState.value = NewsUiState.Error(e.message)
             }
         }
     }
+
+
+    fun searchNews(query: String, country: String, category: String) {
+        viewModelScope.launch {
+            _newsState.value = NewsUiState.Loading
+            try {
+                val result = newsUseCases.searchNews(query, country, category)
+                _newsState.value = NewsUiState.Success(result)
+            } catch (e: Exception) {
+                _newsState.value = NewsUiState.Error(e.message)
+            }
+        }
     }
+}
